be3828b0aedf70e97c7c1c53f67e81ab70ca44d0
triciapearl-0312 <125946806+triciapearl-0312@users.noreply.github.com>
2024-01-02 15:27:32 +0800

    Update streamlit_app.py
    added fruits_to_show = my_fruit_list.loc[fruits_selected] in the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
