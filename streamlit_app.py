95df0d7ea8f3dc0d3c8aa6e475dcbe373901bee1
triciapearl-0312 <125946806+triciapearl-0312@users.noreply.github.com>
2024-01-02 15:20:51 +0800

    Update streamlit_app.py
    added another header for special menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ streamlit.text('🥗 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach, and Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Eggs')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
