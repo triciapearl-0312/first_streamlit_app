f05597ad087c49157224490c3a2018f4b957c6a8
triciapearl-0312 <125946806+triciapearl-0312@users.noreply.github.com>
2024-01-02 15:14:39 +0800

    Update streamlit_app.py
    changed title and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('🥣 Breakfast Menu')
+streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach, and Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Eggs')
